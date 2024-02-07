28f39ddb0e419908ee920950846a4dd3768df9c8
Zokirjon Mamadjonov <zikr.and.mehr@gmail.com>
2024-02-07 20:41:01 +0100

    fixed text field cursor controller focus inconsistency

--- app/src/main/java/com/zikrcode/zikrhelp/presentation/ml_kit/components/ResultContent.kt
+++ app/src/main/java/com/zikrcode/zikrhelp/presentation/ml_kit/components/ResultContent.kt
@@ -4,20 +4,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.zikrcode.zikrhelp.R
 import com.zikrcode.zikrhelp.presentation.utils.Dimens
+import com.zikrcode.zikrhelp.presentation.utils.keyboardAsState
 import com.zikrcode.zikrhelp.ui.theme.ZikrHelpTheme
 
 @Composable
@@ -26,6 +32,12 @@ fun ResultContent(
     result: String?,
     onResultChange: (String) -> Unit
 ) {
+    val focusManager = LocalFocusManager.current
+    val isKeyboardOpen by keyboardAsState()
+    if (!isKeyboardOpen) {
+        focusManager.clearFocus(force = true)
+    }
+
     val clipboardManager = LocalClipboardManager.current
 
     Column(modifier = modifier) {
@@ -39,7 +51,11 @@ fun ResultContent(
             modifier = Modifier.fillMaxWidth(),
             label = {
                 Text(text = stringResource(R.string.result))
-            }
+            },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = {
+                focusManager.clearFocus(force = true)
+            }),
         )
         Spacer(Modifier.height(Dimens.SpacingDouble))
         Button(
